f9f28c2a15f4f4677fb63b36bc1de470c9e2652a
Jose Solis <tucorreo@ejemplo.com>
2025-11-21 21:57:36 -0600

    Agregar directorio include con los archivos mat.h y app.h correspondientes a la seccion Ley de Ohm

---
+++ include/mate.h
@@ -0,0 +1,9 @@
+/* Este header file correspondiente a la función para hacer los calculos relacionados 
+a la ley de ohm utilizando botones para generar una interfaz grafica por medio de gtk */
+#ifndef MATE_H
+#define MATE_H
+#include <gtk/gtk.h>
+
+
+
+#endif

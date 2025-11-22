f21105cbdb3b3e0b8d9afb594c2f0b06e6159570
Jose Solis <tucorreo@ejemplo.com>
2025-11-21 22:46:31 -0600

    Se agrega el headerfile de la parte de la interfaz gráfica

--- include/app.h
+++ include/app.h
@@ -4,6 +4,9 @@ sección de la calculador */
 #define APP_H
 #include <gtk/gtk.h>
 
+// Se declara la función que activa la app
+void on_activate(GtkApplication *app, gpointer user_data);  // Esto define la señal para activar el programa
 
-
+// Se declara la función para aplicar CSS
+void apply_css();   // Esto define el formato css que se utiliza en aplicaciones gráficas, las segmenta por capas
 #endif

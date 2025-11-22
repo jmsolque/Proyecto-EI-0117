d1f875da625988c0fc37e55ef9bb62cce0ea9cc2
Jose Solis <tucorreo@ejemplo.com>
2025-11-21 22:35:49 -0600

    Se agrega el headerfile de la parte matematica

--- include/mate.h
+++ include/mate.h
@@ -4,6 +4,16 @@ a la ley de ohm utilizando botones para generar una interfaz grafica por medio d
 #define MATE_H
 #include <gtk/gtk.h>
 
+// Decalración de funciones generales para la calculadora, estos son los botones que hacen las acciones
+void calcular_ohm(GtkWidget *widget, gpointer data);   //widget es u puntero que sigue la acción de tocar el botón
+void limpiar_campos(GtkWidget *widget, gpointer data);  // data es un puntero generico que envia información adicional del botón
+void enter_calcular(GtkWidget *widget, gpointer data);
+
+// Definición de widgets globales, estos son los espacios donde se meten o salen datos
+extern GtkWidget *tension_in;      
+extern GtkWidget *corriente_in;
+extern GtkWidget *reistencia_in;
+extern GtkWidget *result_label;
 
 
 #endif

ad98f22f594de28a2bfc03413e4ee49ee43a7143
Jose Solis <tucorreo@ejemplo.com>
2025-11-22 19:34:40 -0600

    Se agrega el headerfile correspondient al menu

--- include/menu.h
+++ include/menu.h
@@ -1,7 +1,13 @@
 // En esta sección se tiene el header file para el menu principal de la app
 #ifndef MENU_H
 #define MENU_H
+#include <gtk/gtk.h>
 
+void on_activate_menu(GtkApplication *app, gpointer user_data);
+void apply_css_menu();
+void open_ohm_calculator(GtkWidget *widget, gpointer data);
+void open_resistance_calculator(GtkWidget *widget, gpointer data);
+void open_power_calculator(GtkWidget *widget, gpointer data);
 
 
 #endif 

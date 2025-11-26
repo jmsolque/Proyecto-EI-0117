46b3a67dafbd3bbd3c77813c2256d7cec21efc41
melanyrosales <melanyrosalesm2109@gmail.com>
2025-11-26 17:45:51 +0000

    Agregando estructura inicial del módulo de potencia y comentarios

--- src/pot.c
+++ src/pot.c
@@ -1,25 +1,29 @@
 #include "pot.h"
 #include <gtk/gtk.h>
-#include <stdlib.h>
 
-//  variables globales
-GtkWidget *entry_voltaje;
-GtkWidget *entry_corriente;
-GtkWidget *entry_resistencia;
-GtkWidget *combo_formula;
-GtkWidget *label_resultado;
+// Aquí voy a ir implementando el cálculo de la potencia,
+// pero por ahora dejo las funciones listas para que el módulo compile sin errores.
 
-// Función que calcula la potencia 
+// Variables globales 
+GtkWidget *entry_voltaje = NULL;
+GtkWidget *entry_corriente = NULL;
+GtkWidget *entry_resistencia = NULL;
+GtkWidget *combo_formula = NULL;
+GtkWidget *label_result = NULL;
+
 void calcular_potencia(GtkWidget *widget, gpointer data) {
-    
+    // Aquí más adelante se va a leer los datos y hacer las fórmulas de potencia.
+    // por el momento solo lo dejo como una prueba para ver que la función se llama bien.
+    g_print("calcular_potencia() todavía no está implementada\n");
 }
 
-// Limpia los campos
 void limpiar_campos_pot(GtkWidget *widget, gpointer data) {
-    
+   
+    g_print("limpiar_campos_pot() todavía no está lista\n");
 }
 
-// Para calcular cuando se presione enter
-void enter_calcular_pot(GtkWidget *entry, gpointer data) {
-   
+void enter_calcular_pot(GtkWidget *entry, gpointer user_data) {
+    // Esta función permitirá calcular al presionar enter.
+    
+    g_print("ENTER presionado en módulo potencia (todavía sin lógica)\n");
 }
--- src/poteapp.c
+++ src/poteapp.c
@@ -2,9 +2,23 @@
 #include "pot.h"
 #include <gtk/gtk.h>
 
-// Esta función crea la ventana del módulo de potencia
-void activate_potencia_calculator(GApplication *app, gpointer user_data) {
+// Aquí viy a ir construyendo la pantalla del módulo de potencia
 
-    // Luego se gregaran widgets, cajas, botones
-   
+
+void activate_potencia_calculator(GApplication *app, gpointer user_data)
+{
+    // Creo la ventana principal del módulo de potencia
+    GtkWidget *window = gtk_application_window_new(GTK_APPLICATION(app));
+    gtk_window_set_title(GTK_WINDOW(window), "Calculadora - Potencia");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+    // Caja principal vertical donde luego voy a meter los campos y botones
+    GtkWidget *main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_window_set_child(GTK_WINDOW(window), main_box);
+
+    // Texto temporal solo para saber que sí abre la ventana
+    GtkWidget *label_temp = gtk_label_new("Aquí irá el módulo de potencia :)");
+    gtk_box_append(GTK_BOX(main_box), label_temp);
+
+    gtk_widget_show(window);
 }

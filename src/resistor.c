b4fa5bb00883ae7f8a3e634661f5bd016817fe12
SitoH17 <randallalonso17@gmail.com>
2025-11-26 17:15:29 -0600

    Agregando la parte de calculo de la resistencia mas los datos

--- src/resistor.c
+++ src/resistor.c
@@ -1,40 +1,71 @@
-#include <gtk/gtk.h>
+ #include <gtk/gtk.h>
+#include <math.h>
 
-//Estructura base app
+//Estructura base App
+
 typedef struct {
-    GtkWidget *main_window;
+    GtkWidget *combo1;
+    GtkWidget *combo2;
+    GtkWidget *combo3;
+    GtkWidget *combo4;
+    GtkWidget *label_resultado;
 } AppData;
 
+//     Datos de la resistencia
 
-//Funcion principal 
-void app_activate(GApplication *app, gpointer user_data)
+const char *colores[] = {
+    "Negro", "Marrón", "Rojo", "Naranja", "Amarillo",
+    "Verde", "Azul", "Violeta", "Gris", "Blanco"
+};
+
+const double multiplicadores[] = {
+    1, 10, 100, 1000, 10000,
+    100000, 1000000, 10000000, 100000000, 1000000000
+};
+
+const char *tolerancias_texto[] = {
+    "±1%", "±2%", "±5%", "±10%"
+};
+
+const double tolerancias_valores[] = {
+    1, 2, 5, 10
+};
+
+//Función de Cálculo
+void actualizar_valor(AppData *data)
 {
-    AppData *data = g_new0(AppData, 1);
+    int b1 = gtk_combo_box_get_active(GTK_COMBO_BOX(data->combo1));
+    int b2 = gtk_combo_box_get_active(GTK_COMBO_BOX(data->combo2));
+    int mult = gtk_combo_box_get_active(GTK_COMBO_BOX(data->combo3));
+    int tol  = gtk_combo_box_get_active(GTK_COMBO_BOX(data->combo4));
 
-    // Crear la ventana
-   data->main_window = gtk_application_window_new(GTK_APPLICATION(app));
-    gtk_window_set_title(GTK_WINDOW(data->main_window), "Calculadora de Resistencias");
-    gtk_window_set_default_size(GTK_WINDOW(data->main_window), 600, 400);
+    int dig1 = b1;
+    int dig2 = b2;
+    double mul = multiplicadores[mult];
+    double resultado = (dig1 * 10 + dig2) * mul;
 
-    // Un label placeholder
-    GtkWidget *label = gtk_label_new("Interfaz inicial lista...");
-    gtk_window_set_child(GTK_WINDOW(data->main_window), label);
+    const char *tol_txt = tolerancias_texto[tol];
 
-    gtk_window_present(GTK_WINDOW(data->main_window));
+    char buffer[128];
+
+    if (resultado >= 1000000)
+        snprintf(buffer, sizeof(buffer), "%.2f MΩ %s", resultado / 1e6, tol_txt);
+    else if (resultado >= 1000)
+        snprintf(buffer, sizeof(buffer), "%.2f kΩ %s", resultado / 1e3, tol_txt);
+    else
+        snprintf(buffer, sizeof(buffer), "%.0f Ω %s", resultado, tol_txt);
+
+    gtk_label_set_text(GTK_LABEL(data->label_resultado), buffer);
 }
 
-
-// Funcion main
-int main(int argc, char *argv[])
+//Evento de cambio
+void on_combo_changed(GtkComboBox *widget, gpointer user_data)
 {
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("org.gtk.resistorcalc", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
-
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-
-    return status;
+    actualizar_valor((AppData *)user_data);
 }
+
+// Creación de combo
+GtkWidget *crear_combo_colores()
+{
+    GtkWidget *combo = gtk_combo_box_t_*
+

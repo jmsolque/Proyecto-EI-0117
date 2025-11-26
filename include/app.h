e93dd8b655e47450b33b03c97c8f9ff434aff9f0
jmsolque <josemanuel.solis@ucr.ac.cr>
2025-11-26 00:35:53 -0600

    Se añade la función para regresar al menu

--- include/app.h
+++ include/app.h
@@ -5,9 +5,10 @@ sección de la calculador */
 #include <gtk/gtk.h>
 
 // Se declara la función que activa la app
-void on_activate(GtkApplication *app, gpointer user_data);  // Esto define la señal para activar el programa
+void on_activate(GtkApplication *app, gpointer user_data);
 
 // Se declara la función para aplicar CSS
-void apply_css();   // Esto define el formato css que se utiliza en aplicaciones gráficas, las segmenta por capas
+void apply_css_ohm(void);  // CAMBIAR NOMBRE
+
 void volver_al_menu(GtkWidget *widget, gpointer data);
-#endif
+#endif 

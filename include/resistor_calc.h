cd3b4443ded2c5460221f896a4f8716a3861a154
SitoH17 <randallalonso17@gmail.com>
2025-11-26 20:28:06 -0600

    Cambiando Variables

--- include/resistor_calc.h
+++ include/resistor_calc.h
@@ -1,8 +1,11 @@
+
 #ifndef RESISTOR_CALC_H
 #define RESISTOR_CALC_H
 
+
 #include <gtk/gtk.h>
 #include <math.h>
+
 
 // Estructura principal de la aplicación
 typedef struct {
@@ -16,16 +19,24 @@ typedef struct {
     GtkWidget *combo2;
     GtkWidget *combo3;
     GtkWidget *combo4;
+    GtkWidget *window; // Nueva referencia a la ventana
 } AppData;
+
 
 // Declaración de funciones
 void actualizar_valor(AppData *data);
 void on_color_changed(GtkComboBox *widget, gpointer user_data);
 GtkWidget* crear_combo_colores();
-void aplicar_tema_css();
+void aplicar_tema_css_resistor(); // Cambiado el nombre
 void app_activate(GApplication *app, gpointer user_data);
+
 
 // Dibujo del resistor
 void draw_resistor(GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data);
+
+
+// Función para volver al menú (nueva)
+void volver_menu_resistor(GtkWidget *widget, gpointer user_data);
+
 
 #endif // RESISTOR_CALC_H
